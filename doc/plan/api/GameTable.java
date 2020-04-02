308c362334bcd2fcc355521cebee654404dc57b3
Connor Penny <cgp19@duke.edu>
2020-04-02 19:38:37 -0400

    More APIs in plan added

---
+++ doc/plan/api/GameTable.java
@@ -0,0 +1,20 @@
+package api;
+
+public interface GameTable {
+
+    /***
+     *This method will create the buttons and styling for a given game with data
+     * read from styling files for the general features of casino games
+     * (Private methods will be added to break this up and more APIs may be implemented
+     * where necessary)
+     */
+    void makeGameScene();
+
+    /***
+     * This method will generate the buttons that are necessary for every game
+     * such as betting buttons, clear bet, and deal/spin
+     */
+    void createGamePlayButtons();
+
+
+}
--- doc/plan/api/Menu.java
+++ doc/plan/api/Menu.java
@@ -2,8 +2,18 @@ package api;
 
 public interface Menu  {
 
-    void CreateGameButtons();
+    /***
+     * This method will allow for the creation of a button specific to a certain game,
+     * and will obtain styling for the button from a style folder
+     */
+    void CreateGameButton();
 
-    void
+    /***
+     * This method will be used to set the functionality of a game button to
+     * create the new scene with game elements specific to any game
+     * by implementing a specific GameTable object and making its scene. It will instantiate a new
+     * Player and pass that to the GameTable.
+     */
+    void StartGame();
 
 }
