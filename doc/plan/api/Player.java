0612288f37f65b982c1ede77046a84f8ff77f17e
Alex Angel <apa17@duke.edu>
2020-04-02 19:01:08 -0400

    Update Player.java

--- doc/plan/api/Player.java
+++ doc/plan/api/Player.java
@@ -14,7 +14,6 @@ public interface Player  {
     This method reduces the player bankroll by the amount of a bet.
     Throws an exception if the player does not have enough money to place that bet.
     Called by the Bet class.
-    Marks the status of player as bet pending when called.
     @param amount - int containing the value to be subtracted from balance.
     */
     void placeBet(int amount);
@@ -22,11 +21,8 @@ public interface Player  {
     /*
     Adds an amount of money to bankroll is case a bet was won. Amount supplied
     by a Bet object.
-    Throws an exception if there was not a bet pending (to prevent cheating or
-    errors).
     @param amount - int contaning the value to be added to the player
     representing the amount in the bankroll
-    Removes the bet pending status on player.
     Registers a won bet for statistics purposes.
     */
     void betWon(int amount);
