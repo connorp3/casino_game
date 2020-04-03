f97173faa058d63638c8fa92f7dc1aafb2eabb98
Connor Penny <cgp19@duke.edu>
2020-04-02 20:07:31 -0400

    Trying to work out making use case examples

--- doc/plan/api/Backend/Bet.java
+++ doc/plan/api/Backend/Bet.java
@@ -5,4 +5,6 @@ public interface Bet {
 
     void generatePayout();
 
+    void addBet(int amount);
+
 }
--- doc/plan/usecases/UseCase1.java
+++ doc/plan/usecases/UseCase1.java
@@ -1,6 +1,12 @@
 package usecases;
 
-public class UseCase1 {
+import api.Backend.Bet;
 
-    
+// after playing round of game, user wins an outcome
+class ExampleBet implements Bet {
+    int bet
+    @Override
+    public void generatePayout() {
+
+    }
 }
