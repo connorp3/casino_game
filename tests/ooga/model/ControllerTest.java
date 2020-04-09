73d12ebea42d83bfae74b90ec4129a8c94c75432
Alex <apa17@duke.edu>
2020-04-09 18:05:23 -0400

    Added test for Bet and Player - 100% line coverage for Bet and 80% line coverage for Player

---
+++ tests/ooga/model/ControllerTest.java
@@ -0,0 +1,12 @@
+package ooga.model;
+
+public class ControllerTest {
+
+
+    public static final String SLOTS = "SLOTS";
+    Player player1 = new Player(1000, SLOTS);
+    Bet bet1 = new Bet(player1);
+
+    
+
+    }
---
+++ tests/ooga/model/SlotMachineTest.java
@@ -0,0 +1,16 @@
+package ooga.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SlotMachineTest {
+
+
+    public static final String SLOTS = "SLOTS";
+    Player player1 = new Player(1000, SLOTS);
+    Bet bet1 = new Bet(player1);
+
+
+
+    }
